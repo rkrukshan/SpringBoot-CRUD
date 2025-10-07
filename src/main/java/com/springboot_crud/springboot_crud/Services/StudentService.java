084588e12b9bc296bbcef91bb18d4cbd49bcc7c5
rkrukshan <rukshan1122@gmail.com>
2025-10-07 22:50:56 +0530

    Create Student Service

--- src/main/java/com/springboot_crud/springboot_crud/Services/StudentService.java
+++ src/main/java/com/springboot_crud/springboot_crud/Services/StudentService.java
@@ -1,4 +1,17 @@
 package com.springboot_crud.springboot_crud.Services;
 
+import com.springboot_crud.springboot_crud.Dtos.StudentDto;
+
+import java.util.List;
+
 public interface StudentService {
+    StudentDto createStudent(StudentDto dto);
+
+    StudentDto getStudentById(Integer sid);
+
+    List<StudentDto> getAllStudents();
+
+    StudentDto updateStudent(Integer sid, StudentDto dto);
+
+    void deleteStudent(Integer sid);
 }
