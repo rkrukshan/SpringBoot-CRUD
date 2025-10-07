a8bb5075b8c9ba1f6c7917828f8af3a52c13b3f5
rkrukshan <rukshan1122@gmail.com>
2025-10-07 21:51:16 +0530

    Create Student Model

--- src/main/java/com/springboot_crud/springboot_crud/Models/Student.java
+++ src/main/java/com/springboot_crud/springboot_crud/Models/Student.java
@@ -1,4 +1,26 @@
 package com.springboot_crud.springboot_crud.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int sid;
+    private String name;
+    private int age;
+    private String email;
+    private String department;
+    private String gpa;
+    private int phno;
+    private String address;
 }
