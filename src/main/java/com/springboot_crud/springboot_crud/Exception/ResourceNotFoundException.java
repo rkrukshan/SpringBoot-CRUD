16fc79d9ab30ed34c279da926068372092b4d3e1
rkrukshan <rukshan1122@gmail.com>
2025-10-07 22:16:54 +0530

    Create ResourceNotFoundException

--- src/main/java/com/springboot_crud/springboot_crud/Exception/ResourceNotFoundException.java
+++ src/main/java/com/springboot_crud/springboot_crud/Exception/ResourceNotFoundException.java
@@ -1,4 +1,9 @@
 package com.springboot_crud.springboot_crud.Exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends Exception {
+
+    ResourceNotFoundException(String message){
+        super(message);
+    }
+
 }
