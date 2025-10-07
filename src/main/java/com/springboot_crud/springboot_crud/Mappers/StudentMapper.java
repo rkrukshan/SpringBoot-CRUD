1c8e28bdf0a8e83978e4c1018cfc06e395290a23
rkrukshan <rukshan1122@gmail.com>
2025-10-07 22:06:21 +0530

    Create Student Mapper

--- src/main/java/com/springboot_crud/springboot_crud/Mappers/StudentMapper.java
+++ src/main/java/com/springboot_crud/springboot_crud/Mappers/StudentMapper.java
@@ -1,4 +1,32 @@
 package com.springboot_crud.springboot_crud.Mappers;
 
+import com.springboot_crud.springboot_crud.Dtos.StudentDto;
+import com.springboot_crud.springboot_crud.Models.Student;
+
 public class StudentMapper {
+public static StudentDto entitytodto (Student student){
+                        return new StudentDto(
+                                student.getSid(),
+                                student.getName(),
+                                student.getAge(),
+                                student.getEmail(),
+                                student.getDepartment(),
+                                student.getGpa(),
+                                student.getPhno(),
+                                student.getAddress()
+                        );
+}
+
+public static Student dtotoentity (StudentDto dto){
+                        return new  Student(
+                                dto.getSid(),
+                                dto.getName(),
+                                dto.getAge(),
+                                dto.getEmail(),
+                                dto.getDepartment(),
+                                dto.getGpa(),
+                                dto.getPhno(),
+                                dto.getAddress()
+                        );
+}
 }
