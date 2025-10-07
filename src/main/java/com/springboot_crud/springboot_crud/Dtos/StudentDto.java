0ba79cc7cf1e572fc4d0dfe74f872ba19a626a43
rkrukshan <rukshan1122@gmail.com>
2025-10-07 21:55:35 +0530

    Create Student Dto

--- src/main/java/com/springboot_crud/springboot_crud/Dtos/StudentDto.java
+++ src/main/java/com/springboot_crud/springboot_crud/Dtos/StudentDto.java
@@ -1,4 +1,19 @@
 package com.springboot_crud.springboot_crud.Dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentDto {
+    private int sid;
+    private String name;
+    private int age;
+    private String email;
+    private String department;
+    private String gpa;
+    private int phno;
+    private String address;
 }
