26484b3ba8ae72d826b43d31095745222d14c07a
rkrukshan <rukshan1122@gmail.com>
2025-10-07 21:53:37 +0530

    Create Student Repository

--- src/main/java/com/springboot_crud/springboot_crud/Repositories/StudentRepository.java
+++ src/main/java/com/springboot_crud/springboot_crud/Repositories/StudentRepository.java
@@ -1,4 +1,7 @@
 package com.springboot_crud.springboot_crud.Repositories;
 
-public interface StudentRepository {
+import com.springboot_crud.springboot_crud.Models.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student,Integer> {
 }
