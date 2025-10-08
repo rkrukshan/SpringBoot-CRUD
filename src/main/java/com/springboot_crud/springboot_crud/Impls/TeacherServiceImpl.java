4ebf5996e0aecf24a1170980c3e0ce84d12347a6
rkrukshan <rukshan1122@gmail.com>
2025-10-08 17:09:27 +0530

    Create Teacher ServiceImpl

--- src/main/java/com/springboot_crud/springboot_crud/Impls/TeacherServiceImpl.java
+++ src/main/java/com/springboot_crud/springboot_crud/Impls/TeacherServiceImpl.java
@@ -1,4 +1,54 @@
 package com.springboot_crud.springboot_crud.Impls;
 
-public class TeacherServiceImpl {
+import com.springboot_crud.springboot_crud.Dtos.TeacherDto;
+import com.springboot_crud.springboot_crud.Exception.ResourceNotFoundException;
+import com.springboot_crud.springboot_crud.Mappers.TeacherMapper;
+import com.springboot_crud.springboot_crud.Models.Teacher;
+import com.springboot_crud.springboot_crud.Repositories.TeacherRepository;
+import com.springboot_crud.springboot_crud.Services.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class TeacherServiceImpl implements TeacherService {
+
+    @Autowired
+    TeacherRepository repository;
+    @Override
+    public TeacherDto createTeacher(TeacherDto dto) {
+        Teacher teacher= TeacherMapper.dtotoentity(dto);
+        repository.save(teacher);
+        return TeacherMapper.entitytodto(teacher);
+    }
+
+    @Override
+    public TeacherDto getTeacherById(Integer tid) {
+        Teacher teacher = repository.findById(tid).orElseThrow(()->new ResourceNotFoundException("ID is not Found "+tid));
+        return TeacherMapper.entitytodto(teacher);
+    }
+
+    @Override
+    public List<TeacherDto> getAllTeacher() {
+        return repository.findAll().stream().map(TeacherMapper::entitytodto).collect(Collectors.toList());
+    }
+
+    @Override
+    public TeacherDto updateTeacher(Integer tid, TeacherDto dto) {
+        Teacher teacher = repository.findById(tid).orElseThrow(()->new ResourceNotFoundException("ID is not Found "+tid));
+        teacher.setName(dto.getName());
+        teacher.setAddress(dto.getAddress());
+        teacher.setPhno(dto.getPhno());
+        teacher.setSubject(dto.getSubject());
+        teacher.setDepartment(dto.getDepartment());
+        return TeacherMapper.entitytodto(repository.save(teacher));
+    }
+
+    @Override
+    public void deleteTeacher(Integer tid) {
+    Teacher teacher = repository.findById(tid).orElseThrow(()->new ResourceNotFoundException("ID is not Found "+tid));
+    repository.deleteById(tid);
+    }
 }
