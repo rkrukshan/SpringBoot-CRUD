32cd9aedc262fb58db58749f387da483900f2fad
rkrukshan <rukshan1122@gmail.com>
2025-10-08 16:14:33 +0530

    Create Teacher Dto

--- src/main/java/com/springboot_crud/springboot_crud/Dtos/TeacherDto.java
+++ src/main/java/com/springboot_crud/springboot_crud/Dtos/TeacherDto.java
@@ -1,4 +1,17 @@
 package com.springboot_crud.springboot_crud.Dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TeacherDto {
+    private int tid;
+    private String name;
+    private String address;
+    private long phno;
+    private String subject;
+    private String department;
 }
