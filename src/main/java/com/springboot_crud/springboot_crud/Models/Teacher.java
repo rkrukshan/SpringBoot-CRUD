d81f6fd7197f4dfc01a2fb888c9972a413c6b58f
rkrukshan <rukshan1122@gmail.com>
2025-10-08 16:13:42 +0530

    Create Teacher Model

--- src/main/java/com/springboot_crud/springboot_crud/Models/Teacher.java
+++ src/main/java/com/springboot_crud/springboot_crud/Models/Teacher.java
@@ -1,4 +1,25 @@
 package com.springboot_crud.springboot_crud.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Teacher {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int tid;
+    private String name;
+    private String address;
+    private long phno;
+    private String subject;
+    private String department;
+
 }
