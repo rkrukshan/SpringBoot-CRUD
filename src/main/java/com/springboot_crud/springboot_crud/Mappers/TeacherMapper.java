8b5af80b400e45d897d74ab213f9363855cebca5
rkrukshan <rukshan1122@gmail.com>
2025-10-08 16:21:26 +0530

    Create Teacher Mapper

--- src/main/java/com/springboot_crud/springboot_crud/Mappers/TeacherMapper.java
+++ src/main/java/com/springboot_crud/springboot_crud/Mappers/TeacherMapper.java
@@ -1,4 +1,28 @@
 package com.springboot_crud.springboot_crud.Mappers;
 
+import com.springboot_crud.springboot_crud.Dtos.TeacherDto;
+import com.springboot_crud.springboot_crud.Models.Teacher;
+
 public class TeacherMapper {
+    public static TeacherDto entitytodto(Teacher teacher){
+        return new TeacherDto(
+                teacher.getTid(),
+                teacher.getName(),
+                teacher.getAddress(),
+                teacher.getPhno(),
+                teacher.getSubject(),
+                teacher.getDepartment()
+        );
+    }
+
+    public static Teacher dtotoentity(TeacherDto dto){
+        return new Teacher(
+                dto.getTid(),
+                dto.getName(),
+                dto.getAddress(),
+                dto.getPhno(),
+                dto.getSubject(),
+                dto.getDepartment()
+        );
+    }
 }
